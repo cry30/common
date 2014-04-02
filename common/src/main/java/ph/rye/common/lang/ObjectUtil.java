a2a0c365c80efe0a24b3a960db6e5cdbeea217ca
royce.com@gmail.com <royce.com@gmail.com@c4100dec-d0d7-eea9-9859-db99657ce98a>
2014-04-02 01:12:11 +0000

    dev

--- common/src/main/java/ph/rye/common/lang/ObjectUtil.java
+++ common/src/main/java/ph/rye/common/lang/ObjectUtil.java
@@ -1,3 +1,18 @@
+/**
+ *   Copyright 2014 Royce Remulla
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package ph.rye.common.lang;
 
 import java.util.Map;
@@ -54,6 +69,84 @@ public class ObjectUtil {
         } else {
             retval = object1.equals(object2);
         }
+        return retval;
+    }
+
+    /**
+     * Return first parameter if non-null, otherwise return the second
+     * parameter.
+     * 
+     * @param ifObj return if non-null;
+     * @param elseObj return if null.
+     * @param <T> generic method, any type of object.
+     */
+    public <T> T nvl(final T ifObj, final T elseObj)
+    {
+        T retval;
+
+        if (ifObj == null) {
+            retval = elseObj;
+        } else {
+            retval = ifObj;
+        }
+        return retval;
+    }
+
+    /**
+     * PLSQL decode function.
+     * 
+     * @param expression is the value to compare. S
+     * @param search is the value that is compared against expression. S
+     * @param result is the value returned, if expression is equal to search.
+     *            default is optional. T
+     * 
+     * @param <S> Search parameter type.
+     * @param <T> Result type.
+     * 
+     * @see Unit Test: PrsUtil_decodeTest.
+     */
+    @SuppressWarnings({
+            "PMD.CyclomaticComplexity",
+            "unchecked",
+            "PMD.CompareObjectsWithEquals" //Required to compare nulls.
+    })
+    public <S, T> T decode(final S expression, final S search,
+            final T... result)
+    {
+
+        final java.util.List<S> ifList = new java.util.ArrayList<S>();
+        ifList.add(search);
+
+        final java.util.List<T> thenList = new java.util.ArrayList<T>();
+
+        T defaultResult = null; //NOPMD: null default, conditionally redefine.
+        if (result == null || result.length == 0) {
+            thenList.add(null);
+
+        } else {
+            if (result.length % 2 == 0) {
+                defaultResult = result[result.length - 1];
+            }
+
+            for (int i = 0; i < result.length; i++) {
+                if (i % 2 == 1 && i != result.length - 1) {
+                    ifList.add((S) result[i]);
+                } else if (i % 2 == 0) {
+                    thenList.add(result[i]);
+                }
+
+            }
+        }
+        T retval = defaultResult; //NOPMD: initialize by default, conditionally redefine.
+        for (int i = 0; i < ifList.size(); i++) {
+            final S nextIf = ifList.get(i);
+            if (expression == nextIf || nextIf != null
+                    && nextIf.equals(expression)) {
+                retval = thenList.get(i);
+                break;
+            }
+        }
+
         return retval;
     }
 
---
+++ common/src/main/java/ph/rye/common/lang/ResourceUtil.java
@@ -0,0 +1,57 @@
+/**
+ *   Copyright 2014 Royce Remulla
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package ph.rye.common.lang;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Resource bundle utility.
+ * 
+ * <pre>
+ * $Author$ 
+ * $Date$
+ * </pre>
+ */
+public class ResourceUtil {
+
+
+    /**
+     * Reads a String array using the indexed key.
+     * 
+     * @param resource resource bundle name. Not null.
+     * @param key resource key. Not null.
+     */
+    public String[] getArray(final String resource, final String key)
+    {
+        assert resource != null;
+        assert key != null;
+
+        final List<String> retval = new ArrayList<String>();
+        new AbstractResource() {
+
+            @Override
+            protected void process(final String string, final int... index)
+            {
+                retval.add(string);
+            }
+        }.execute(resource, key);
+
+        return retval.toArray(new String[retval.size()]);
+    }
+
+}
